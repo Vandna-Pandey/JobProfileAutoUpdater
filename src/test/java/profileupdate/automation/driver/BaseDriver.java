873c7b92b74b9ee8ae35a1d2596b906d091e1db8
Vandna Pandey <63224926+Vandna-Pandey@users.noreply.github.com>
2026-03-08 23:31:41 +0530

    Update BaseDriver.java

--- src/test/java/profileupdate/automation/driver/BaseDriver.java
+++ src/test/java/profileupdate/automation/driver/BaseDriver.java
@@ -3,6 +3,7 @@ package profileupdate.automation.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 
 public class BaseDriver {
 
