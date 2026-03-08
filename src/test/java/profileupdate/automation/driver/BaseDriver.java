ef17531881ba40f5778cd9a7b2c77e2f3d60a306
Vandna-Pandey <vandnapandey1997@gmail.com>
2026-03-08 23:47:49 +0530

    Test

--- src/test/java/profileupdate/automation/driver/BaseDriver.java
+++ src/test/java/profileupdate/automation/driver/BaseDriver.java
@@ -3,7 +3,6 @@ package profileupdate.automation.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Dimension;
 
 public class BaseDriver {
 
@@ -15,8 +14,7 @@ public class BaseDriver {
 
             WebDriverManager.chromedriver().setup();
             driver = new ChromeDriver();
-            //driver.manage().window().maximize();
-            driver.manage().window().setSize(new Dimension(1920,1080));
+            driver.manage().window().maximize();
 
         }
 
